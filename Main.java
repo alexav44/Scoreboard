3656772d0c2219f402d415911b39130334441be8
alexav44 <alexav44@nycstudents.net>
2024-10-01 16:13:43 +0000

    Added the example statement

--- Main.java
+++ Main.java
@@ -7,7 +7,35 @@ public class Main{
         System.out.println(info);
     game.recordPlay(1); 
     info = game.getScore();
-    System.out.println(info);        
+    System.out.println(info);
+
+    game.recordPlay(0);
+    info = game.getScore();
+    System.out.println(info);
+
+    
+    info = game.getScore();
+    System.out.println(info);
+    info = game.getScore();
+
+    game.recordPlay(3);
+    info = game.getScore();
+    game.recordPlay(1);
+    game.recordPlay(0);
+    System.out.println(info);
+    info = game.getScore();
+    game.recordPlay(0);
+    System.out.println(info);
+    game.recordPlay(4);
+    game.recordPlay(0);
+    info = game.getScore();
+    System.out.println(info);
+    Scoreboard match = new Scoreboard("Lions", "Tigers");
+    info= match.getScore();
+    System.out.println(info);
+    info = game.getScore();
+    System.out.println(info);
+
     }  
      
     
